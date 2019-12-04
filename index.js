7505b2b588a4f26dae1fd5d0c30fda161d84b02c
Kevin Carr <kevincarr@Kevins-MacBook-Pro.local>
2019-12-04 17:14:31 -0600

    wrote endpoint for when the client makes a GET request to /api/users

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require("express")
+const db = require("./data/db")
+
+const server = express()
+
+server.use(express.json())
+
+server.get("/users", (req, res) => {
+  if (db) {
+    res.json(db)
+  } else {
+    res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+  }
+})
